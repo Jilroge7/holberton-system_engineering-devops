a9066d4bca3393eb475e924c2566df79fe76c60f
Jilroge7 <1672@holbertonschool.com>
2020-09-15 03:43:55 +0000

    adding task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-""" Return todo list progress for a emp from a REST API """
+""" Exports a emp from a REST API to CSV """
 if __name__ == "__main__":
     import csv
     import requests
     import sys
 
+    user_id = sys.argv[1]
+    user = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                        user_id).json()
+    u_name = user.get("username")
+    url = 'https://jsonplaceholder.typicode.com/todos/?userId='
+    tasks = requests.get(url + user_id).json()
 
-    employees = (requests.get('https://jsonplaceholder.typicode.com/users/')).json()
-    # for employee in employees.json():
-    if ((employee.get("id")) == (sys.argv[1])):
-        emp_name = employee.get("name")
-
-    tasks = requests.get('https://jsonplaceholder.typicode.com/(sys.argv[1])/todos')
-    for task in tasks.json()
-        print("Employee {} is done with tasks({}/{}):".format(
-              emp_name, len(task), len(tasks.json())))
-        if ((task.get("completed") == True):
-            print("/t {}".format(task.get("title")))
+    with open('{}.csv'.format(user_id), 'w', newline='') \
+            as csvFile:
+        writer = csv.writer(csvFile, quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            writer.writerow([user_id, u_name,
+                            task.get("completed"),
+                            task.get("title")])
