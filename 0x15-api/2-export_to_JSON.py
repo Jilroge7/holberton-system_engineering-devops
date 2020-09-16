e50a910c2b3dbe5a2183d270e617f105e6990daf
Jilroge7 <1672@holbertonschool.com>
2020-09-16 15:15:38 +0000

    edit to task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
-""" Return todo list progress for a emp from a REST API """
+""" Exports a emp from a REST API to JSON """
 if __name__ == "__main__":
+    import json
     import requests
     import sys
 
     user_id = sys.argv[1]
-    employees = requests.get('https://jsonplaceholder.typicode.com/users/' +
-                             user_id).json()
-    emp_name = (employees.get("name"))
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(user_id)).json()
+    u_name = user.get("username")
     url = 'https://jsonplaceholder.typicode.com/todos/?userId='
     tasks = requests.get(url + user_id).json()
-    tasks_done = []
+    jlist = []
+    ldict = {}
     for task in tasks:
-        if (task.get("completed")) is True:
-            tasks_done.append(task.get("title"))
-    print("Employee {} is done with tasks({}/{}):".format(
-          emp_name, len(tasks_done), len(tasks)))
-    for d_task in tasks_done:
-        print("\t {}".format(d_task))
+        jdict = {}
+        jdict["task"] = task.get("title")
+        jdict["completed"] = task.get("completed")
+        jdict["username"] = u_name
+        jlist.append(jdict)
+    ldict[user_id] = jlist
+
+    with open('{}.json'.format(user_id), 'w') as f:
+        fil = json.dumps(ldict)
+        f.write(fil)
