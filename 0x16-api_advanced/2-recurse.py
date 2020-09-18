655a14717d7020e7b2141d9b2a6d7b3e743c0943
Jilroge7 <1672@holbertonschool.com>
2020-09-18 04:22:59 +0000

    adding task 2 partial

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" get top ten posts """
+import requests
+
+
+def top_ten(subreddit):
+    """ function to retrieve top ten posts for subreddit """
+
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    about = requests.get(url, headers={"User-Agent": "User"},
+                         allow_redirects=False)
+    about = about.json()
+    outdict = about.get("data")
+    child = outdict.get("children")
+    try:
+        for key in child:
+            insdict = key.get("data")
+            toplist = insdict.get("title")
+            print(toplist)
+        return
+    except:
+        print(None)
+        return
