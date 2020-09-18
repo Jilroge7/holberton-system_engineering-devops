e98b79c5455b8ceb887ba77b23c3ddec22747f8e
Jilroge7 <1672@holbertonschool.com>
2020-09-18 04:10:19 +0000

    adding task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """ get top ten posts """
 import requests
-from pprint import pprint
 
 
 def top_ten(subreddit):
     """ function to retrieve top ten posts for subreddit """
 
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     about = requests.get(url, headers={"User-Agent": "User"},
-                         allow_redirects=False).json()
+                         allow_redirects=False)
+    about = about.json()
+    outdict = about.get("data")
+    child = outdict.get("children")
     try:
-        for posts in about:
-            while len(posts < 10):
-                top = about["data"].get("title")
-                print(top)
+        for key in child:
+            insdict = key.get("data")
+            toplist = insdict.get("title")
+            print(toplist)
+        return
     except:
         print(None)
+        return
