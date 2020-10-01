7909c54791a6c73fa3eba3781e42657a1b246940
Jilroge7 <1672@holbertonschool.com>
2020-10-01 15:59:44 +0000

    adding commas

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # automated puppet script to correct 500 error for Wordpress page.
 exec { 'find and replace':
-    command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php"
-    path    => [ '/usr/bin' , '/bin', '/usr/sbin', '/sbin' ]
+    command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+    path    => [ '/usr/bin' , '/bin', '/usr/sbin', '/sbin' ],
 
 }
