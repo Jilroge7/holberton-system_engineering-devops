b23ee1d2bcd3ea2aac8a83977f449f6ace11011c
Jilroge7 <1672@holbertonschool.com>
2020-10-01 15:33:37 +0000

    removing extra statements from puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,18 +1,4 @@
 # automated puppet script to correct 500 error for Wordpress page.
-exec { 'open file':
-    command => 'vi /var/www/html/wp-settings.php'
-}
-
 exec { 'find and replace':
-    command => sed -i 's/.phpp/.php/g' wp-settings.php
-    require => Exec['open file']
-}
-
-package { 'apache2':
-    ensure => 'installed'
-}
-
-exec { 'restart':
-    command => 'sudo service apache2 restart'
-    require => Package['apache2']
+    command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php"
 }
