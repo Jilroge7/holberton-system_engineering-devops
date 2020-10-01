623555dbc26d766b1557c90121687ca4f264b827
Jilroge7 <1672@holbertonschool.com>
2020-10-01 14:56:45 +0000

    adding content to task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,2 +1,18 @@
 # automated puppet script to correct 500 error for Wordpress page.
+exec { 'open file':
+    command => 'vi /var/www/html/wp-settings.php'
+}
 
+exec { 'find and replace':
+    command => sed -i 's/.phpp/.php/g' wp-settings.php
+    require => Exec['open file']
+}
+
+package { 'apache2':
+    ensure => 'installed'
+}
+
+exec { 'restart':
+    command => 'sudo service apache2 restart'
+    require => Package['apache2']
+}
