5abfd24ac04b7174dd1d686dc93d45ba8412f7d0
Jilroge7 <1672@holbertonschool.com>
2020-10-01 15:51:42 +0000

    edit to add path for sed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
 # automated puppet script to correct 500 error for Wordpress page.
 exec { 'find and replace':
     command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php"
+    path    => [ '/usr/bin' , '/bin', '/usr/sbin', '/sbin' ]
 }
